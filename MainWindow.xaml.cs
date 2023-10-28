833a6da4493da511a4df51e7097391ad7b595ff6
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-10-28 17:11:42 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR_21_102_Plotnikov_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.SizeChanged += new TextResize(this).Window_SizeChanged;
+
+            tb_input.TextChanged += Tb_input_TextChanged;
+        }
+
+        //Вывод данных
+        private void print(string msg)
+        {
+            tb_result.Text = msg;
+        }
+
+        //Проверка введённого числа
+        private void Tb_input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string input = tb_input.Text;      
+            long input_number = -1;
+
+            //Проверка введённых данных на правильность
+            if (!long.TryParse(input, out input_number))
+            {
+                print("Введённые данные не являются целым числом");
+                return;
+            }
+
+            if (input_number <= 0)
+            {
+                print("Число должно быть неотрицательным");
+                return;
+            }
+
+            try
+            {
+                //Финальная проверка введённых данных на правильность
+                if (input.Length != 12 || input.StartsWith('0'))
+                {
+                    print("Введённое число должно быть 12-значным");
+                    return;
+                }
+
+                //Поиск произведения первых 3 и суммы последних 9
+                int firstMult = 1;
+                int lastSum = 0;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    firstMult *= int.Parse(input[i].ToString());
+                }
+                for (int i = 3; i < 12; i++)
+                {
+                    lastSum += int.Parse(input[i].ToString());
+                }
+
+                //Вывод полученного ответа
+                if (firstMult == lastSum)
+                {
+                    print("Произведение первых 3 десятичных цифр равно сумме 9 последних десятичных цифр");
+                }
+                else
+                {
+                    print("Произведение первых 3 десятичных цифр НЕ равно сумме 9 последних десятичных цифр");
+                }
+            }
+            catch (Exception ex)
+            {
+                //обработка ошибки
+                MessageBox.Show("Не удалось проверить число. Ошибка: " + ex.Message, "Ошибка программы", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ TextResize.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PR_21_102_Plotnikov_1
+{
+    /// <summary>
+    /// Реализует изменение размера текста элементов интерфейса окна
+    /// </summary>
+    internal class TextResize
+    {
+        Window window; //Цеевое окно (в нём меняет размер текста)
+
+        public TextResize(Window window)
+        {
+            this.window = window;
+        }
+
+        /// <summary>
+        /// Передавать делегату окна (обычно слушатель: при изменении размера)
+        /// </summary>
+        public void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var textboxes = FindVisualChildren<TextBox>(window);
+            var textBlocks = FindVisualChildren<TextBlock>(window);
+
+            var NewTextSize = (e.NewSize.Height / 20.0 + e.NewSize.Width / 10.0) / 2;
+            if (NewTextSize > 48) NewTextSize = 48;
+
+            foreach (var textbox in textboxes)
+            {
+                textbox.FontSize = NewTextSize / 1.5;
+            }
+
+            foreach (var textblock in textBlocks)
+            {
+                textblock.FontSize = NewTextSize / 2;
+            }
+        }
+
+        /// <summary>
+        /// возвращает список всех элементов интерфейса данного окна заданного типа
+        /// </summary>
+        /// <typeparam name="T"> тип искомых элементов интерфейса</typeparam>
+        /// <param name="parent">окно</param>
+        /// <returns></returns>
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+        {
+            //Все элементы окна
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child != null && child is T)
+                {
+                    yield return (T)child;
+                }
+
+                //Дочерние элементы элементов
+                foreach (T nestedChild in FindVisualChildren<T>(child))
+                {
+                    yield return nestedChild;
+                }
+            }     
+        }
+    }
+}
